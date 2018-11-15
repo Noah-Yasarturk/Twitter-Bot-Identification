c6417b66c130751c424dac143e3f2565f316a7af
Noah-Yasarturk <40246941+Noah-Yasarturk@users.noreply.github.com>
2018-11-15 08:09:44 -0500

    Update Stream3.py

--- Stream3.py
+++ Stream3.py
@@ -23,10 +23,10 @@ This runs 1st in our data pipeline:
 
 
 #Stream tweets
-consumer_key="GVHhMXYiyj1fHExAu3Bbda3i9"
-consumer_secret="j2IP4ocmoqesE9HS9yMey1HROXj6C6MIlnJds2zv2TMacPLVEJ"
-access_token="798242515833016321-xAAnsVIMJU1rP5cEpmVXxwQE0Jhdw0p"
-access_token_secret="DkkwatnPO6sI9JIIhogAAXDNPy34mcZrxtbu3W7EOPDOO"
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_token_secret=""
 
 
 class StdOutListener(StreamListener):
