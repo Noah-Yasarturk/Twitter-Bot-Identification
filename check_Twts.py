9bc3a2c0d9775e1850d82e9e5a2296816f7bba26
Noah Yasarturk <40246941+Noah-Yasarturk@users.noreply.github.com>
2019-05-17 16:13:13 -0400

    Update check_Twts.py
    Removed auth
    Now in Data Mining (Classes) folder & Python Scripts

--- check_Twts.py
+++ check_Twts.py
@@ -33,10 +33,10 @@ they are botlike.
 
 def main():
     #Auth api
-    consumer_key="GVHhMXYiyj1fHExAu3Bbda3i9"
-    consumer_secret="j2IP4ocmoqesE9HS9yMey1HROXj6C6MIlnJds2zv2TMacPLVEJ"
-    access_token="798242515833016321-xAAnsVIMJU1rP5cEpmVXxwQE0Jhdw0p"
-    access_token_secret="DkkwatnPO6sI9JIIhogAAXDNPy34mcZrxtbu3W7EOPDOO"
+    consumer_key=""
+    consumer_secret=""
+    access_token=""
+    access_token_secret=""
     
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
